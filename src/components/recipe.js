250c97664fea54a1bbe79f1b51e02fa562290124
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-09-17 00:12:15 +0100

    Updated the homepage

---
+++ src/components/recipe.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'
+
+export default function Recipe() {
+  
+    return (
+      <View className="mx-4 space-y-3 ">
+        <Text style={{fontSize: hp(3)}} className="font-semibold text-gray-700"> Recipe </Text>
+      </View>
+    )
+  
+// nvcc
+
+}
